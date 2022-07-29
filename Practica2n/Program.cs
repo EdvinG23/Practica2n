5ad2b92c25c1f7bc93ee985b4812595f899fbe16
MINEDUCYT <MINEDUCYT@DESKTOP-O47S48O>
2022-07-29 09:02:42 -0600

    Agregar archivos de proyecto.

---
+++ Practica2n/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica2n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
