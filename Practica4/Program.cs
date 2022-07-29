fa9761248c5884a426a8c6dbf14034f610a30167
MINEDUCYT <MINEDUCYT@DESKTOP-O47S48O>
2022-07-29 10:08:32 -0600

    Tercer programa

---
+++ Practica4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Practica4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 52.24F;
+            double num2 = 52.24;
+            decimal num3 = 52.24M;
+
+            Console.WriteLine("{0} {1} {2}", num1, num2, num3);
+        }
+    }
+}
