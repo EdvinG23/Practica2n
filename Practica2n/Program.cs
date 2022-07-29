a753c57805d6eba1b6c552eea02b959e03c18d24
MINEDUCYT <MINEDUCYT@DESKTOP-O47S48O>
2022-07-29 09:53:45 -0600

    Segundo programa

--- Practica2n/Program.cs
+++ Practica2n/Program.cs
@@ -6,7 +6,16 @@ namespace Practica2n
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declaracion de variables
+
+            int num1 = 5;
+            int num2 = 4;
+            var total = num1 + num2;
+            string name = "Edvin";
+            int edad = 27;
+
+            Console.WriteLine("La suma es de: " + total);
+            Console.WriteLine(name + " " + edad);
         }
     }
 }
---
+++ Practica3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practica3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaracion de variables
+            string firstname = "Edvin";
+            String lastname = "Guevara";
+            int edad = 18;
+            // Concatenacion de variables
+            var name = firstname + " " + lastname;
+            // Imprimir variables dentro del texto
+            Console.WriteLine("Su nombre es: {1} y su edad es: {0}", edad, name);
+        }
+    }
+}
